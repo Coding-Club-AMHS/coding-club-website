f9ca3b10eba5a7b36f4cb9bb90654e700e90054d
minamete <348563206@gapps.yrdsb.ca>
2021-03-10 16:35:51 -0500

    Added styled components

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
+import Container from 'react-bootstrap/Container';
 
 export default function Home() {
   return (
@@ -9,7 +10,9 @@ export default function Home() {
       </Head>
 
       <main className={styles.main}>
-        
+        <Container>
+            <h1>Coding Club AMHS</h1>
+        </Container>
       </main>
 
       <footer className={styles.footer}>
