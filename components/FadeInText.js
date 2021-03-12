0d10f65bcccef4a4af57150c71f23fad5389a567
Joyce Ye <46504694+yejoyce8@users.noreply.github.com>
2021-03-11 19:08:16 -0500

    fadeintext

---
+++ components/FadeInText.js
@@ -0,0 +1,8 @@
+import React, {useEffect, useState} from 'react';
+import ReactDOM from 'react-dom';
+
+import styles from '../styles/Home.module.css'
+
+const FadeInText= () => {
+    const [isVisible]
+}
