0243204eb84b66d303f4e5ab39fec92886c47cbc
minamete <348563206@gapps.yrdsb.ca>
2021-03-13 18:23:13 -0500

    fixed broken navbar

--- pages/index.js
+++ pages/index.js
@@ -5,21 +5,23 @@ import Navbar from '../components/Navbar';
 import IntroScreen from '../components/IntroScreen';
 
 const DefaultHome = () => {
-    return (
-      <div className={styles.container} id={styles.unloaded} onClick={() => window.location='/home'}>
-        {/*Changes background colour*/}
-        <Head>
-          <title>Coding Club AMHS</title>
-          <link rel="icon" type="image/png" href="/favicon.png" />
-        </Head>
-  
-        <main className={styles.main}>
-            <Navbar isDark={true}/>
-            {/* loads initial screen */} 
-            <IntroScreen />
-        </main>
-      </div>
-    )
+  return (
+    <div className={styles.container} id={styles.unloaded}>
+      {/*Changes background colour*/}
+      <Head>
+        <title>Coding Club AMHS</title>
+        <link rel="icon" type="image/png" href="/favicon.png" />
+      </Head>
+
+      <main className={styles.main}>
+        <Navbar isDark={true} />
+        {/* loads initial screen */}
+        <div onClick={() => window.location = '/home'}>
+          <IntroScreen />
+        </div>
+      </main>
+    </div>
+  )
 }
 
 export default DefaultHome;
